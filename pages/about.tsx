90c7537bba25001baf2612329d5644c42d761684
ryo-i <iidazz0109@gmail.com>
2022-01-30 13:09:50 +0900

    ホワイトアルバム2枚目を追加 #1

--- pages/about.tsx
+++ pages/about.tsx
@@ -53,7 +53,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>ホワイトアルバム1枚目を追加 <a href="https://github.com/ryo-i/beatles-db/issues/1">#1</a></li>
+                    <li>ホワイトアルバム2枚目を追加 <a href="https://github.com/ryo-i/beatles-db/issues/1">#1</a></li>
                     <li>Realtime DBのデータをfetchで読み込んでconsoleに表示 <a href="https://github.com/ryo-i/beatles-db/issues/3">#3</a></li>
                 </ul>
             </section>
