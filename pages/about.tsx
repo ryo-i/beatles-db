8bd17e8193a0c227daf59ca2fa3b58fef35e4429
ryo-i <iidazz0109@gmail.com>
2022-01-30 10:57:37 +0900

    8thアルバムの入力終了 #1

--- pages/about.tsx
+++ pages/about.tsx
@@ -53,7 +53,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>7thアルバムの入力終了 <a href="https://github.com/ryo-i/beatles-db/issues/1">#1</a></li>
+                    <li>8thアルバムの入力終了 <a href="https://github.com/ryo-i/beatles-db/issues/1">#1</a></li>
                     <li>Realtime DBのデータをfetchで読み込んでconsoleに表示 <a href="https://github.com/ryo-i/beatles-db/issues/3">#3</a></li>
                 </ul>
             </section>
