ca6d437a2b4f01eb71969e4c46946526525a73c8
ryo-i <iidazz0109@gmail.com>
2022-02-17 05:41:09 +0900

    動的ページをページ構成のレイアウトに変更 #3

--- pages/track/[no].jsx
+++ pages/track/[no].jsx
@@ -1,10 +1,41 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Data from '../../data/data.json';
 
+
+// Component
 const Post = () => {
   const router = useRouter();
   const { no } = router.query;
 
-  return <p>track: {no}</p>;
-};
+  const headerTitle = Data.header.title;
+  const pageTitle = no;
+  const pageText = no + 'の詳細情報';
+  const headTitle = pageTitle + ' | ' + headerTitle;
+
+    return (
+        <>
+        <Head>
+            <title>{ headTitle }</title>
+            <meta name="description" content={ pageText } />
+            <meta property="og:title" content={ headTitle } />
+            <meta property="og:description" content={ pageText } />
+        </Head>
+
+        <Header />
+        <main>
+            <h1>{ pageTitle }</h1>
+            <p dangerouslySetInnerHTML={{ __html: pageText }}></p>
+            <section>
+                <h2>「{no}」について</h2>
+                <p>説明です。説明です。説明ですったら説明です。</p>
+            </section>
+        </main>
+        <Footer />
+        </>
+    );
+}
 
 export default Post;
