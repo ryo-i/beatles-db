0c7bc626a3b1af8fbe3eeeae5f91fb2fc0667a34
ryo-i <iidazz0109@gmail.com>
2022-08-23 07:41:59 +0900

    検索窓を追加（機能はまだない） #3

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,6 +24,31 @@ const HeaderTag = styled.header`
   nav span, nav a {
     padding-right: 0.5em;
   }
+  .search {
+    margin: 15px 0 0;
+    display: flex;
+    justify-content: center;
+    input {
+      width: 200px;
+      margin: 0;
+      height: 30px;
+      padding: 5px;
+      font-size: 14px;
+      color: #333;
+      border: 1px solid #ccc;
+    }
+    button {
+      padding: 6px;
+      line-height: 1em;
+      font-size: 14px;
+      color: #fff;
+      border: 1px solid #999;
+      background: #666;
+      :hover {
+        cursor: pointer;
+      }
+    }
+  }
 `;
 
 
@@ -39,6 +64,10 @@ function Header() {
           <Link href="/"><a>Home</a></Link>
           <Link href="/about"><a>About</a></Link>
         </nav>
+        <form className="search">
+          <input type="text" />
+          <button type="button">検索</button>
+        </form>
       </div>
     </HeaderTag>
   );
